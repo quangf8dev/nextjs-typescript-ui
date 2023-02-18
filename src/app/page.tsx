1c4190e3d1c2870310622d04507a991cf2290803
bird <quang82thcspb@gmail.com>
2023-02-18 15:50:56 +0700

    add SCSS NEXT

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from "../../Home.module.scss";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.title}>Trang chu </div>
+        </main>
+    );
+}
